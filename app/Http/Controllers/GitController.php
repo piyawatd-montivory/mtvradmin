cb2daef4cedd4f093f0f8af75717a323fe372fdb
Piyawat <arunwatd@gmail.com>
2022-06-29 09:22:22 +0700

    test git command

---
+++ app/Http/Controllers/GitController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\File;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Http;
+use Response;
+
+class GitController extends Controller
+{
+    function index()
+    {
+        exec("git pull origin master");
+        return true;
+    }
+}
+?>
