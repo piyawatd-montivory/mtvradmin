84c90c3abec4aa87c1722ecd645d1735ad63ff32
Piyawat <arunwatd@gmail.com>
2022-07-07 16:53:26 +0700

    add nullable

--- database/migrations/2022_07_01_011016_create_contacts_table.php
+++ database/migrations/2022_07_01_011016_create_contacts_table.php
@@ -17,11 +17,11 @@ return new class extends Migration
             $table->id();
             $table->string('contact_type');
             $table->text('fullname');
-            $table->integer('select_position');
-            $table->string('company');
+            $table->integer('select_position')->nullable(true);
+            $table->string('company')->nullable(true);
             $table->string('phone');
             $table->string('email');
-            $table->text('cv');
+            $table->text('cv')->nullable(true);
             $table->text('message')->nullable();
             $table->boolean('status_mail')->default(0);
             $table->text('free_field')->nullable();
