c5ddbc5fc3d2014aa88e57ba64e7b310880cacab
Piyawat <arunwatd@gmail.com>
2022-07-08 12:16:20 +0700

    add position and linkedln url

--- resources/views/components/card-people.blade.php
+++ resources/views/components/card-people.blade.php
@@ -4,9 +4,9 @@
     </div>
     <div class="card-meta">
         <h3 class="card-title people-name">{{ $fullname }}</h3>
-        <p class="card-subtitle people-description">Lorem ipsum dolor sit amet</p>
+        <p class="card-subtitle people-description">{{ $position }}</p>
         <div class="people-link">
-            <a href="#"><i class="ic ic-linkedin"></i></a>
+            <a href="{{ $linkedinurl }}"><i class="ic ic-linkedin"></i></a>
         </div>
     </div>
 </div>
