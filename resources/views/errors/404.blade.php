54a94fd2f4878c8ef3b93654651f9131f5c5b7e6
Piyawat <arunwatd@gmail.com>
2022-07-11 15:58:16 +0700

    add 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,11 +7,22 @@
 @section('stylesheet')
 @endsection
 @section('content')
-<div class="container mt-3">
-    <div class="row-10">
-        contact admin
-    </div>
-</div>
+<main id="content">
+
+    <section class="error404">
+        <div class="container">
+            <div class="sc-inner">
+                <div class="error-content">
+                    <h2 class="error-text">404</h2>
+                    <h2 class="error-text">PAGE NOT FOUND</h2>
+                </div>
+                <a href="{{ route('home') }}" class="btn">BACK TO HOMEPAGE</a>
+                <img alt="" src="{{ asset('/images/frontend/404.jpg')}}" class="error-img">
+            </div>
+        </div>
+    </section>
+
+</main>
 @endsection
 @section('scripts')
 @endsection
