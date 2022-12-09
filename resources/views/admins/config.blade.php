4f9f4c64d1569d63bbfc1fe3cfc9125efd7de3aa
Piyawat <arunwatd@gmail.com>
2022-12-09 12:39:48 +0700

    add result config page

---
+++ resources/views/admins/config.blade.php
@@ -0,0 +1,17 @@
+@extends('admins.template.template')
+@section('title')
+Config
+@endsection
+@section('stylesheet')
+@endsection
+@section('content')
+<div class="container-fluid px-4">
+    <div class="row mt-3">
+        <div class="col-6">
+            <h1>Generate Cache Success</h1>
+        </div>
+    </div>
+</div>
+@endsection
+@section('script')
+@endsection
