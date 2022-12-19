931fa59eabea6b5747a90442ab4238336c6cdfe9
Piyawat <arunwatd@gmail.com>
2022-12-19 14:28:32 +0700

    build filter style

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -16,6 +16,7 @@ Montivory Category
 @endsection
 @section('stylesheet')
 <link rel="stylesheet" href="{{asset('/css/blog-category.css')}}" type="text/css">
+<!-- Latest compiled and minified CSS -->
 <style>
 
 </style>
@@ -30,7 +31,78 @@ Montivory Category
     </div>
 </section>
 <section class="content">
-    <div class="container">
+    <div class="container pt-4">
+        <div class="row">
+            <div class="col-12 d-none d-md-block">
+                <div class="row">
+                    <div class="col-3">
+                        <div class="dropdown dropdown-month">
+                            <button class="btn custom-dropdown dropdown-toggle w-100 text-start dropdownMonth" type="button" id="dropdownMonth" data-bs-toggle="dropdown" aria-expanded="false">
+                                All months
+                            </button>
+                            <ul class="dropdown-menu w-100" aria-labelledby="dropdownMonth">
+                            <li><a class="dropdown-item month-select check-selected" href="javascript:void(0);" data="all">All months</a></li>
+                            <li><a class="dropdown-item month-select" href="javascript:void(0);" data="january">January</a></li>
+                            <li><a class="dropdown-item month-select" href="javascript:void(0);" data="february">February</a></li>
+                            <li><a class="dropdown-item month-select" href="javascript:void(0);" data="march">March</a></li>
+                            <li><a class="dropdown-item month-select" href="javascript:void(0);" data="april">April</a></li>
+                            <li><a class="dropdown-item month-select" href="javascript:void(0);" data="may">May</a></li>
+                            <li><a class="dropdown-item month-select" href="javascript:void(0);" data="june">June</a></li>
+                            <li><a class="dropdown-item month-select" href="javascript:void(0);" data="july">July</a></li>
+                            <li><a class="dropdown-item month-select" href="javascript:void(0);" data="august">August</a></li>
+                            <li><a class="dropdown-item month-select" href="javascript:void(0);" data="september">September</a></li>
+                            <li><a class="dropdown-item month-select" href="javascript:void(0);" data="november">November</a></li>
+                            <li><a class="dropdown-item month-select" href="javascript:void(0);" data="december">December</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="dropdown dropdown-year">
+                            <button class="btn custom-dropdown dropdown-toggle w-100 text-start dropdownYear" type="button" id="dropdownYear" data-bs-toggle="dropdown" aria-expanded="false">
+                                All years
+                            </button>
+                            <ul class="dropdown-menu w-100" aria-labelledby="dropdownYear">
+                            <li><a class="dropdown-item year-select check-selected" href="javascript:void(0);" data="all">All years</a></li>
+                            <li><a class="dropdown-item year-select" href="javascript:void(0);" data="2022">2022</a></li>
+                            <li><a class="dropdown-item year-select" href="javascript:void(0);" data="2021">2021</a></li>
+                            <li><a class="dropdown-item year-select" href="javascript:void(0);" data="2022">2020</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="dropdown dropdown-year">
+                            <button class="btn custom-dropdown dropdown-toggle w-100 text-start" type="button" id="dropdownTag" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+                                All tags
+                            </button>
+                            <ul class="dropdown-menu w-100" aria-labelledby="dropdownTag">
+                                <li>
+                                    <a class="dropdown-item tag-select d-none" data="all"><input type="checkbox" class="form-check-input" id="alltag" value="all" label="All tags" checked>All tags</a>
+                                    <a class="dropdown-item tag-clear" href="javascript:cleartag();">CLEAR</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item tag-select" data="all"><input type="checkbox" class="form-check-input" value="valtag1" label="Tag 1">Tag 1</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item tag-select" data="all"><input type="checkbox" class="form-check-input" value="tag2" label="Tag 2">Tag 2</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item tag-select" data="all"><input type="checkbox" class="form-check-input" value="tag3" label="Tag 3">Tag 3</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn btn-readnow px-3" type="button" >APPLY</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 d-md-none">
+                <button class="btn px-3 btn-outline-filter" type="button" data-bs-toggle="modal" data-bs-target="#filterModal">
+                    <img src="{{asset('images/icon/Filter-icon.png')}}" class="filter-icon"/>
+                    FILTER
+                </button>
+            </div>
+        </div>
         <div class="row">
             <div class="col-12 col-md-4 content-block">
                 <img src="{{asset('images/default/ArticleTeaser.jpg')}}" class="card-img-top" alt="...">
@@ -77,11 +149,161 @@ Montivory Category
         </div>
     </div>
 </section>
+<!-- Modal -->
+<div class="modal fade" id="filterModal" tabindex="-1" aria-labelledby="filterModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-fullscreen">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="filterModalLabel">Filter</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-12 mb-3">
+                            <div class="dropdown dropdown-month">
+                                <button class="btn custom-dropdown dropdown-toggle w-100 text-start ps-0 dropdownMonth" type="button" id="dropdownMonth" data-bs-toggle="dropdown" aria-expanded="false">
+                                    All months
+                                </button>
+                                <ul class="dropdown-menu w-100" aria-labelledby="dropdownMonth">
+                                <li><a class="dropdown-item month-select check-selected" href="javascript:void(0);" data="all">All months</a></li>
+                                <li><a class="dropdown-item month-select" href="javascript:void(0);" data="january">January</a></li>
+                                <li><a class="dropdown-item month-select" href="javascript:void(0);" data="february">February</a></li>
+                                <li><a class="dropdown-item month-select" href="javascript:void(0);" data="march">March</a></li>
+                                <li><a class="dropdown-item month-select" href="javascript:void(0);" data="april">April</a></li>
+                                <li><a class="dropdown-item month-select" href="javascript:void(0);" data="may">May</a></li>
+                                <li><a class="dropdown-item month-select" href="javascript:void(0);" data="june">June</a></li>
+                                <li><a class="dropdown-item month-select" href="javascript:void(0);" data="july">July</a></li>
+                                <li><a class="dropdown-item month-select" href="javascript:void(0);" data="august">August</a></li>
+                                <li><a class="dropdown-item month-select" href="javascript:void(0);" data="september">September</a></li>
+                                <li><a class="dropdown-item month-select" href="javascript:void(0);" data="november">November</a></li>
+                                <li><a class="dropdown-item month-select" href="javascript:void(0);" data="december">December</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-12 mb-3">
+                            <div class="dropdown dropdown-year">
+                                <button class="btn custom-dropdown dropdown-toggle w-100 text-start ps-0 dropdownYear" type="button" id="dropdownYear" data-bs-toggle="dropdown" aria-expanded="false">
+                                    All years
+                                </button>
+                                <ul class="dropdown-menu w-100" aria-labelledby="dropdownYear">
+                                <li><a class="dropdown-item year-select check-selected" href="javascript:void(0);" data="all">All years</a></li>
+                                <li><a class="dropdown-item year-select" href="javascript:void(0);" data="2022">2022</a></li>
+                                <li><a class="dropdown-item year-select" href="javascript:void(0);" data="2021">2021</a></li>
+                                <li><a class="dropdown-item year-select" href="javascript:void(0);" data="2022">2020</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="dropdown dropdown-year">
+                                <button class="btn custom-dropdown dropdown-toggle w-100 text-start ps-0 dropdownTag" type="button" id="dropdownTagMobile" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+                                    All tags
+                                </button>
+                                <ul class="dropdown-menu w-100" aria-labelledby="dropdownTagMobile">
+                                    <li>
+                                        <a class="dropdown-item tag-select d-none" data="all"><input type="checkbox" class="form-check-input" id="alltag" value="all" label="All tags" checked>All tags</a>
+                                        <a class="dropdown-item tag-clear" href="javascript:cleartag();">CLEAR</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item tag-select" data="all"><input type="checkbox" class="form-check-input" value="valtag1" label="Tag 1">Tag 1</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item tag-select" data="all"><input type="checkbox" class="form-check-input" value="tag2" label="Tag 2">Tag 2</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item tag-select" data="all"><input type="checkbox" class="form-check-input" value="tag3" label="Tag 3">Tag 3</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <div class="d-grid gap-2 col-12">
+                <button type="button" class="btn btn-primary">APPLY</button>
+            </div>
+        </div>
+      </div>
+    </div>
+</div>
 @endsection
 @section('script')
-<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
 <script type="text/javascript">
 
+    $(function(){
+        $('.month-select').on('click',function(){
+            $('.month-select').removeClass('check-selected');
+            $(this).addClass('check-selected');
+            $('.dropdownMonth').text($(this).text());
+        });
+        $('.year-select').on('click',function(){
+            $('.year-select').removeClass('check-selected');
+            $(this).addClass('check-selected');
+            $('.dropdownYear').text($(this).text());
+        });
+    })
+
+    const tagDropdown = document.getElementById('dropdownTag')
+    tagDropdown.addEventListener('hide.bs.dropdown', function () {
+        let showlabel = ''
+        $.each($('.form-check-input'),function(index,value){
+            if(value.checked){
+                if(showlabel === ''){
+                    showlabel = $(value).attr('label');
+                }else{
+                    let checklabel = $(value).attr('label');
+                    showlabel = `${showlabel}, ${checklabel}`;
+                }
+            }
+        })
+        if(showlabel === ''){
+            $('#alltag').prop("checked",true);
+            showlabel = 'All tags';
+        }
+        $('#dropdownTag').text(showlabel);
+    })
+
+    const tagDropdownMobile = document.getElementById('dropdownTagMobile')
+    tagDropdownMobile.addEventListener('hide.bs.dropdown', function () {
+        let showlabel = ''
+        $.each($('.form-check-input'),function(index,value){
+            if(value.checked){
+                if(showlabel === ''){
+                    showlabel = $(value).attr('label');
+                }else{
+                    let checklabel = $(value).attr('label');
+                    showlabel = `${showlabel}, ${checklabel}`;
+                }
+            }
+        })
+        if(showlabel === ''){
+            $('#alltag').prop("checked",true);
+            showlabel = 'All tags';
+        }
+        $('#dropdownTagMobile').text(showlabel);
+    })
+
+    const cleartag = () => {
+        $.each($('.form-check-input'),function(index,value){
+            $(value).prop("checked",false);
+        });
+        $('#alltag').prop("checked",true);
+    }
+
+    const filtercontent = () => {
+        let filter = {}
+        let tags = []
+        $.each($('.form-check-input'),function(index,value){
+            if(value.checked){
+                tags.push(value.defaultValue);
+            }
+        })
+        filter.tags = tags;
+
+    }
 
 </script>
 @endsection
--- resources/views/layouts/javascript-blog.blade.php
+++ resources/views/layouts/javascript-blog.blade.php
@@ -1,2 +1,2 @@
 <script src="{{asset('js/jquery-3.6.0.min.js')}}" type="text/javascript"></script>
-<script src="{{asset('js/bootstrap.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/bootstrap.bundle.min.js')}}" type="text/javascript"></script>
